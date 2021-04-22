7d5ec15d2fb6194987c1a3b5d6c8bd0aebf8c330
Chenyun Zhang <morningchenyun@gmail.com>
2021-04-22 09:44:35 -0400

    tset

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Error() {
     return (
         <div>
-            <div className="hit-the-floor">Working on it...👷</div>
+            <div className="hit-the-floor">404👷</div>
             <div className="hit-the-floor buttom">HUH???🤷‍♀️</div>
         </div>
     )

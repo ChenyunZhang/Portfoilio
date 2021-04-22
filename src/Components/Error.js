b8b7dec79916cd8f0ccfc5a3977b5a53d4d7bfd1
Chenyun Zhang <morningchenyun@gmail.com>
2021-04-21 23:17:27 -0400

    update error page

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Error() {
     return (
         <div>
-            <div className="hit-the-floor">WIFI???</div>
-            <div className="hit-the-floor buttom">WTF???</div>
+            <div className="hit-the-floor">Working on it...👷</div>
+            <div className="hit-the-floor buttom">WTF???👩‍💻</div>
         </div>
     )
 }
